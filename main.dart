12e7054142cdf542bcf21072b3f3f351c479093f
Mauricio_oliver <98127841+Olivermauricio@users.noreply.github.com>
2022-05-12 22:00:13 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MaterialApp(
+home:Container( 
+  
+  child: Column(
+  children: [
+  Expanded(child: Container(color: Colors.blue,)),
+   Expanded(child: Container(color: Colors.yellow,)),
+  ]
+),
+
+),
+  ));
+  
+}
